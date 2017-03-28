1232086f58c630272d0377ca41426a69dd080d5a
Julien Mialon <mialon.julien@gmail.com>
2017-03-28 13:05:08 +0200

    add sample project

---
+++ PollyNetstandardSample/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Polly;
+
+namespace PollyNetstandardSample
+{
+    public class Class1
+    {
+	    private Policy _policy;
+
+	    public Class1()
+	    {
+		    _policy = Policy.Timeout(() => TimeSpan.FromDays(1));
+	    }
+    }
+}
